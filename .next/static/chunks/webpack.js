161e41634b030a7c4a5a7bd19f9fc2a8d116df6d
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2023-01-30 18:45:48 +0200

    add

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ea572145349cb60"; }
+/******/ 		__webpack_require__.h = function() { return "6e8a0486f55c3244"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
